8e1cc92f14aa1ccb4693e5d5652cd1cb55960db8
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-20 15:34:30 -0800

    feat(home-manager): add hyprland keybindings

---
+++ home-manager/modules/hyprland/binds.nix
@@ -0,0 +1,100 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    bind = [
+      "CTRL,           Q, killactive,"
+      "CTRL,           DELETE, exit,"
+      "CTRL,           L, exec, loginctl lock-session"
+      "$mainMod,       G, togglefloating,"
+      "$mainMod,       P, pin,"
+      "$mainMod,       Z, togglesplit,"
+      "$mainMod,       E, exec, bemoji -cn"
+      "$mainMod,       V, exec, cliphist list | $menu --dmenu | cliphist decode | wl-copy"
+      "$mainMod,       O, exec, hyprpicker -an"
+      "$mainMod,       N, exec, swaync-client -t"
+      "$mainMod,       SPACE , exec, $menu --show drun"
+      "$mainMod,       T, exec, $terminal"
+      "$mainMod,       F, exec, $fileManager"
+      "$mainMod,       M, exec, $music"
+      "$mainMod,       B, exec, $browser"
+      "$mainMod,       P, exec, $passwordManager"
+      ", Print, exec, grimblast --notify --freeze copysave area"
+
+      # Moving focus
+      "$mainMod, left,  movefocus, l"
+      "$mainMod, right, movefocus, r"
+      "$mainMod, up,    movefocus, u"
+      "$mainMod, down,  movefocus, d"
+
+      # Moving focus vim style
+      "$mainMod, h, movefocus, l"
+      "$mainMod, l, movefocus, r"
+      "$mainMod, k, movefocus, u"
+      "$mainMod, j, movefocus, d"
+
+      # Moving windows vim style
+      "$mainMod SHIFT, h,  swapwindow, l"
+      "$mainMod SHIFT, j,  swapwindow, r"
+      "$mainMod SHIFT, k,  swapwindow, u"
+      "$mainMod SHIFT, l,  swapwindow, d"
+
+      # Resizing windows vim style                   X  Y
+      "$mainMod ALT, h, resizeactive, -20 0"
+      "$mainMod ALT, j, resizeactive,  20 0"
+      "$mainMod ALT, k, resizeactive,  0 -20"
+      "$mainMod ALT, l, resizeactive,  0  20"
+
+      # Switching workspaces
+      "$altMod, 1, workspace, 1"
+      "$altMod, 2, workspace, 2"
+      "$altMod, 3, workspace, 3"
+      "$altMod, 4, workspace, 4"
+      "$altMod, 5, workspace, 5"
+      "$altMod, C, workspace, 6"
+      "$altMod, E, workspace, 7"
+      "$altMod, T, workspace, 8"
+      "$altMod, M, workspace, 9"
+      "$altMod, B, workspace, 10"
+
+
+      # Moving active window to workspaces
+      "$altMod SHIFT, 1, movetoworkspacesilent, 1"
+      "$altMod SHIFT, 2, movetoworkspacesilent, 2"
+      "$altMod SHIFT, 3, movetoworkspacesilent, 3"
+      "$altMod SHIFT, 4, movetoworkspacesilent, 4"
+      "$altMod SHIFT, 5, movetoworkspacesilent, 5"
+      "$altMod SHIFT, 6, movetoworkspacesilent, 6"
+      "$altMod SHIFT, 7, movetoworkspacesilent, 7"
+      "$altMod SHIFT, 8, movetoworkspacesilent, 8"
+      "$altMod SHIFT, 9, movetoworkspacesilent, 9"
+      "$altMod SHIFT, 0, movetoworkspacesilent, 10"
+
+      # Scratchpad
+      "$mainMod,       S, togglespecialworkspace,  magic"
+      "$mainMod SHIFT, S, movetoworkspace, special:magic"
+    ];
+
+    # Move/resize windows with mainMod + LMB/RMB and dragging
+    bindm = [
+      "$mainMod, mouse:272, movewindow"
+      "$mainMod, mouse:273, resizewindow"
+    ];
+
+    # Laptop multimedia keys for volume and LCD brightness
+    bindel = [
+      ",XF86AudioRaiseVolume,  exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+      ",XF86AudioLowerVolume,  exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ",XF86AudioMute,         exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ",XF86AudioMicMute,      exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+      "$mainMod, bracketright, exec, brightnessctl s 10%+"
+      "$mainMod, bracketleft,  exec, brightnessctl s 10%-"
+    ];
+
+    # Audio playback
+    bindl = [
+      ", XF86AudioNext,  exec, playerctl next"
+      ", XF86AudioPause, exec, playerctl play-pause"
+      ", XF86AudioPlay,  exec, playerctl play-pause"
+      ", XF86AudioPrev,  exec, playerctl previous"
+    ];
+  };
+}
