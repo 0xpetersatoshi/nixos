0c3eecafaf4357a6d20aef0a79d51ccd4e255b76
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-20 14:11:17 -0800

    feat(home-manager): add home manager packages

---
+++ home-manager/home-packages.nix
@@ -0,0 +1,56 @@
+{ pkgs, ... }: {
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    # Packages in each category are sorted alphabetically
+
+    # Desktop apps
+    _1password-cli
+    _1password-gui
+    brave
+    firefox
+    obsidian
+    spotify
+    telegram-desktop
+    yubioath-flutter
+
+    # CLI utils
+    brightnessctl
+    btop
+    cliphist
+    eza
+    fd
+    fzf
+    ghostty
+    kitty
+    neovim
+    ripgrep
+    starship
+    unzip
+    wget
+    wl-clipboard
+    zinit
+    zip
+    zoxide
+
+    # Programming
+    go
+    python314
+    rustup
+    uv
+
+    # WM stuff
+    hyprpolkitagent
+    libsForQt5.dolphin
+    libsForQt5.xwaylandvideobridge
+    libnotify
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+
+    # Other
+    bemoji
+    nix-prefetch-scripts
+    syncthing
+    syncthingtray
+  ];
+}
