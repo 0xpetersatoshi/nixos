e9954f22da010227a402b2c95e598c4cdc03e48d
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-20 15:04:50 -0800

    feat(home-manager): add stylix module

---
+++ home-manager/modules/stylix.nix
@@ -0,0 +1,71 @@
+{ pkgs, inputs, ... }: {
+  imports = [ inputs.stylix.homeManagerModules.stylix ];
+
+  home.packages = with pkgs; [
+    dejavu_fonts
+    jetbrains-mono
+    noto-fonts
+    noto-fonts-lgc-plus
+    noto-fonts-emoji
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+  ];
+
+  stylix = {
+    enable = true;
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+
+    targets = {
+      neovim.enable = false;
+      waybar.enable = false;
+      wofi.enable = false;
+      hyprland.enable = false;
+      hyprlock.enable = false;
+    };
+
+    cursor = {
+      name = "DMZ-Black";
+      size = 24;
+      package = pkgs.vanilla-dmz;
+    };
+
+    fonts = {
+      emoji = {
+        name = "Noto Color Emoji";
+        package = pkgs.noto-fonts-color-emoji;
+      };
+      monospace = {
+        name = "JetBrains Mono";
+        package = pkgs.jetbrains-mono;
+      };
+      sansSerif = {
+        name = "Noto Sans";
+        package = pkgs.noto-fonts;
+      };
+      serif = {
+        name = "Noto Serif";
+        package = pkgs.noto-fonts;
+      };
+
+      sizes = {
+        terminal = 13;
+        applications = 11;
+      };
+    };
+
+    iconTheme = {
+      enable = true;
+      package = pkgs.papirus-icon-theme;
+      dark = "Papirus-Dark";
+      light = "Papirus-Light";
+    };
+
+    image = pkgs.fetchurl {
+      url = "https://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/media/branch/master/png/gruvbox-dark-blue.png";
+      sha256 = "1jrmdhlcnmqkrdzylpq6kv9m3qsl317af3g66wf9lm3mz6xd6dzs";
+    };
+  };
+}
