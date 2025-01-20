28f7aa425a680f2d6559f7ecaec20ee831fb299a
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-20 13:06:16 -0800

    feat(nixos): add default.nix file

---
+++ nixos/modules/default.nix
@@ -0,0 +1,16 @@
+{
+  imports = [
+    ./audio.nix
+    ./bluetooth.nix
+    ./boot.nix
+    ./env.nix
+    ./home-manager.nix
+    ./hyprland.nix
+    ./kernel.nix
+    ./network.nix
+    ./nix.nix
+    ./timezone.nix
+    ./user.nix
+    ./zram.nix
+  ];
+}
