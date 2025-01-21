a18a80d73a10310bfbe76b8ca44a7a85d55627b9
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-20 16:16:31 -0800

    refactor(hosts): add nvidia-specific configuration

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -12,4 +12,28 @@
   networking.hostName = hostname;
 
   system.stateVersion = stateVersion;
+
+  # NVIDIA configuration
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
 }
