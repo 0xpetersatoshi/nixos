e04a4a791989de41a885dd0b328dcc322866f6d6
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2025-01-21 20:38:23 -0800

    refactor: update security module

--- nixos/modules/security.nix
+++ nixos/modules/security.nix
@@ -1,5 +1,23 @@
+{ pkgs, ... }:
+
 {
-  security.pam.services.login.enableKwallet = true;
+  security.pam.services = {
+    kwallet = {
+      name = "kwallet";
+      enableKwallet = true;
+    };
+
+    login = {
+      enableKwallet = true;
+    };
+  };
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-qt;
+  };
+
   # Necessary for yubico authenticator
   services.pcscd.enable = true;
 }
